165ed62b6d1367be8ae46dc34c3c32cef487a990
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-27 07:52:13 +0000

    added Day3 code to master

--- HackerRank.js
+++ HackerRank.js
@@ -1,3 +1,44 @@
+// Day 3 - Intro to Conditional Statements
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+function main() {
+    var N = parseInt(readLine());
+    //Original Code
+    // if (N % 2 !== 0) {
+      //      console.log("Weird");
+      //  }else if (N % 2 === 0 && N >=2 && N <= 5) {
+      //      console.log("Not Weird");
+      //  }else if (N % 2 === 0 && N >= 6 && N <= 20) {
+      //      console.log("Weird");
+      //  }else if (N % 2 === 0 && N > 20) {
+      //      console.log("Not Weird");
+      //  };    
+        
+    //Re-Factored Code using the Ternary Operator
+        (N % 2 === 0 && ((N > 20) || ((N >= 2) && (N <= 5) ))) ?
+            console.log("Not Weird") : console.log("Weird");
+        }
+
+
 //Day 21
 // Objective 
 // Today we're discussing Generics; be aware that not all languages support this construct, so fewer languages are enabled for this challenge. Check out the Tutorial tab for learning materials and an instructional video!
