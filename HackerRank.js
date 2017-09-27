5f12c5c6f1a8eaf3313ed058bc46e36c3096ce95
Candice Bianca Brown <candicebrown97@gmail.com>
2017-09-27 07:42:04 +0000

    completed HackerRank Day21: Generics

--- HackerRank.js
+++ HackerRank.js
@@ -1,41 +1,61 @@
-// Day 3 - Intro to Conditional Statements
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
+//Day 21
+// Objective 
+// Today we're discussing Generics; be aware that not all languages support this construct, so fewer languages are enabled for this challenge. Check out the Tutorial tab for learning materials and an instructional video!
 
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
+// Task 
+// Write a single generic function named printArray; this function must take an array of generic elements as a parameter (the exception to this is C++, which takes a vector). The locked Solution class in your editor tests your function.
 
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
+// Note: You must use generics to solve this challenge. Do not write overloaded functions.
 
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
+// Input Format
 
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
+// The locked Solution class in your editor will pass different types of arrays to your printArray function.
 
-/////////////// ignore above this line ////////////////////
-function main() {
-    var N = parseInt(readLine());
-    //Original Code
-    // if (N % 2 !== 0) {
-      //      console.log("Weird");
-      //  }else if (N % 2 === 0 && N >=2 && N <= 5) {
-      //      console.log("Not Weird");
-      //  }else if (N % 2 === 0 && N >= 6 && N <= 20) {
-      //      console.log("Weird");
-      //  }else if (N % 2 === 0 && N > 20) {
-      //      console.log("Not Weird");
-      //  };    
-        
-    //Re-Factored Code using the Ternary Operator
-        (N % 2 === 0 && ((N > 20) || ((N >= 2) && (N <= 5) ))) ?
-            console.log("Not Weird") : console.log("Weird");
+// Constraints
+
+// You must have exactly  function named printArray.
+// Output Format
+
+// Your printArray function should print each element of its generic array parameter on a new line.
+
+import java.util.*;
+
+class Printer <T> {
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here 
+    public <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
+    }
+    
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
         }
 
-
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
