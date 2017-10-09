3447f75a7a9480720eedbac32187e3f948584cf5
Candice Bianca Brown <candicebrown97@gmail.com>
2017-10-09 15:49:57 +0000

    completed Day5!

--- day5.js
+++ day5.js
@@ -1,12 +1,68 @@
 //Day 5: Loops
-Day 5: Loops 
-by AvmnuSng
-Tutorial
-Problem
-Submissions
-Leaderboard
-Discussions
-Editorial 
+
+//starting line 28 is my 2nd attempt - that worked, now in correct n x i = n*i  format -- YIPPEE!!!
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for (i = 1; i <= 10; i++) {
+      console.log(""+n+" x "+i+" = "+(n*i)+"");
+        
+   };
+};
+
+Congratulations, you passed the sample test case.
+Click the Submit Code button to run your code against all the test cases.
+
+Input (stdin)
+2
+Your Output (stdout)
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+Expected Output
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
+
+
+Day 5: Loops // first attempt below
+
 Objective 
 In this challenge, we're going to use loops to help us do some simple math. Check out the Tutorial tab to learn more.
 
@@ -140,3 +196,4 @@ Expected Output
 2 x 10 = 20
 Compiler Message
 Wrong Answer
+
